a80562fdaa73d52353b8796ecd831850c2d554c7
Maksim Kuryez <maxkuryez@gmail.com>
2023-03-02 07:08:02 -0800

    Doc fix

--- src/components/pages/documentPage/documentPage.tsx
+++ src/components/pages/documentPage/documentPage.tsx
@@ -10,32 +10,32 @@ const DocumentPage = () => {
       <TypographyStyled variant="h5" component="h1">
         1. The states are separated into 4 slices:
         <br/><br/>
-        Cart: handles the items in cart, using hash map, because this way it is easier to go throught it and edit the cart on every action.
+        Cart: handles the items in the cart, using hash map, because this way it is easier to go through  it and edit the cart on every action.
         <br/>
         Products: handles products that are requested from the API.
         <br/>
-        Errors: handles erros with the requests as well as errors with adding too many products to cart
+        Errors: handles errors with the requests as well as errors with adding too many products to cart
         <br/>
         Loading: handles loading screen when making a request.
         <br/><br/>
         2. Handling Thunks:
         <br/><br/>
-        The Products Slice is requested from the API, and is saved to the state,
+        The Products Slice is requested from the API and is saved to the state,
         when you switch to other page and then return back, the thunk that handles the request does not make another request,
-        because it checks that the products are alredy in the state.
+        because it checks that the products are already in the state.
         <br/>
-        This slice is not stored using presist store, because it makes sense that the products will be fetch when the page is reloaded,
+        This slice is not stored using persist store, because it makes sense that the products will be fetch when the page is reloaded,
         but once loaded, there is no need to fetch again.
         <br/><br/>
-        The Cart Slice is stored in the persist store, so the user does not loose the products that were added to the cart.
+        The Cart Slice is stored in persist store, so the user does not loose the products that were added to the cart.
         <br/>
-        When adding the product from the page to cart, it checks whether the limit of max products per type and per qunatity
+        When adding the product from the page to cart, it checks whether the limit of max products per type and per quantity
         is not reached, and if it is, it will throw an error and will not add product to cart.
         <br />
-        This is handled by Thunk, that checks the cart state and then returns an error if needed. and then both Error and Cart
+        This is handled by Thunk, that checks the cart state and then returns an error if needed. And then both Error and Cart
         Slices handle the rejected/fulfilled state of the thunk respectively.
         <br /><br />
-        The limits can be changed in the constants.ts file. When changig the limit of max products per type, it will
+        The limits can be changed in the utils/constants file. When changing the limit of max products per type, it will
         also change the number of products that can be selected in cart, using drop down selector.
         <br /><br />
         Note, that this thunk does not trigger the loading screen, because it is not added to the loading screen thunk handlers.
