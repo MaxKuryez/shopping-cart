36d45eaecf27d6cd0c9c8c10f260298d3cea5c54
Maksim Kuryez <maxkuryez@gmail.com>
2023-06-07 10:26:42 +0100

    test

--- src/components/modules/drawerProduct/drawerProduct.tsx
+++ src/components/modules/drawerProduct/drawerProduct.tsx
@@ -18,6 +18,7 @@ interface CartProductProps {
   product: CartProductType;
 };
 
+//CHANGE HERE
 const DrawerProduct = ({ product }: CartProductProps) => {
   const dispatch = useTypedDispatch();
   const MAX_TITLE_LENGTH = 20;
